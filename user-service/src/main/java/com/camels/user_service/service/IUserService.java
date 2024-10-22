9e2afab5d9974abe960a541ec30be07e3a0c80cc
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-10-23 02:34:51 +0300

    create IUserService and its impl

---
+++ user-service/src/main/java/com/camels/user_service/service/IUserService.java
@@ -0,0 +1,12 @@
+package com.camels.user_service.service;
+
+import com.camels.user_service.dto.SaveUserDto;
+import com.camels.user_service.dto.UpdateUserDto;
+import com.camels.user_service.entity.User;
+
+public interface IUserService {
+    SaveUserDto saveUser(SaveUserDto saveUserDto);
+    UpdateUserDto updateUser(UpdateUserDto updateUserDto);
+    User getMyProfile(Long id);
+    void deleteUser(Long id);
+}
---
+++ user-service/src/main/java/com/camels/user_service/service/impl/UserService.java
@@ -0,0 +1,48 @@
+package com.camels.user_service.service.impl;
+
+import com.camels.user_service.dto.SaveUserDto;
+import com.camels.user_service.dto.UpdateUserDto;
+import com.camels.user_service.entity.User;
+import com.camels.user_service.repository.IUserRepository;
+import com.camels.user_service.service.IUserService;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
+public class UserService implements IUserService {
+
+    private final IUserRepository userRepository;
+    private final ModelMapper modelMapper;
+
+    @Override
+    public SaveUserDto saveUser(SaveUserDto saveUserDto) {
+        User user = modelMapper.map(saveUserDto, User.class);
+        User savedUser = userRepository.save(user);
+        return modelMapper.map(savedUser, SaveUserDto.class);
+    }
+
+    @Override
+    public UpdateUserDto updateUser(UpdateUserDto updateUserDto) {
+        User user = userRepository.findById(updateUserDto.getId()).get();
+        user.setFirstName(updateUserDto.getFirstName());
+        user.setLastName(updateUserDto.getLastName());
+        user.setEmail(updateUserDto.getEmail());
+        user.setPassword(updateUserDto.getPassword());
+        user.setPhone(updateUserDto.getPhone());
+
+        user = userRepository.save(user);
+        return modelMapper.map(user, UpdateUserDto.class);
+    }
+
+    @Override
+    public User getMyProfile(Long id) {
+        return userRepository.findById(id).get();
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+}
