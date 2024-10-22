d6808d3cac765079037e05fb2d518618fc4a585e
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-10-23 02:35:09 +0300

    create UserController

---
+++ user-service/src/main/java/com/camels/user_service/controller/UserController.java
@@ -0,0 +1,42 @@
+package com.camels.user_service.controller;
+
+import com.camels.user_service.dto.SaveUserDto;
+import com.camels.user_service.dto.UpdateUserDto;
+import com.camels.user_service.entity.User;
+import com.camels.user_service.service.IUserService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    private final IUserService userService;
+
+    @PostMapping("/save-user")
+    public ResponseEntity<SaveUserDto> saveUser(@RequestBody SaveUserDto saveUserDto) {
+        SaveUserDto savedUserDto = userService.saveUser(saveUserDto);
+        return savedUserDto != null ? ResponseEntity.ok(savedUserDto) : ResponseEntity.notFound().build();
+    }
+
+    @PostMapping("/update-user")
+    public ResponseEntity<UpdateUserDto> updateUser(@RequestBody UpdateUserDto updateUserDto) {
+        UpdateUserDto updatedUserDto = userService.updateUser(updateUserDto);
+        return updatedUserDto != null ? ResponseEntity.ok(updatedUserDto) : ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getMyProfile(@PathVariable(name = "id") Long id) {
+        User user = userService.getMyProfile(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteUser(@PathVariable(name = "id") Long id) {
+        userService.deleteUser(id);
+        return "Deleted User";
+    }
+
+}
