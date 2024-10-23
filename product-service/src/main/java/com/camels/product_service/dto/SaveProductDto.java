9af27cb1e3a85acf6a4df13e1ea5a758c72e771e
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-10-23 19:26:24 +0300

    create ProductDto,SaveProductDto,UpdateProductDto

---
+++ product-service/src/main/java/com/camels/product_service/dto/SaveProductDto.java
@@ -0,0 +1,21 @@
+package com.camels.product_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SaveProductDto {
+    private Long categoryId;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private Integer stockQuantity;
+    private String imageUrl;
+}
